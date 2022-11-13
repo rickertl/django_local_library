b27630b7b189764a2d2404b873b487f0916914dc
Rick Ertl <ertl.rick@gmail.com>
2022-11-12 19:05:05 -0600

    added custom domain

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -31,7 +31,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ## For example, for a site URL at 'web-production-3640.up.railway.app'
 ## (replace the string below with your own site URL):
-ALLOWED_HOSTS = ['web-production-2aff.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['library.quackt.com', 'web-production-2aff.up.railway.app', '127.0.0.1']
 
 # During development, you can instead set just the base URL
 # (you might decide to change the site a few times).
